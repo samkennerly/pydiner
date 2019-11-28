2f29e47809198adbd76cd32e458ae306997dcef8
Sam Kennerly <samkennerly@gmail.com>
2019-11-27 22:59:24 -0500

    Autoformat with Black

--- test/fixtures.py
+++ test/fixtures.py
@@ -5,17 +5,21 @@ from pathlib import Path
 from shutil import rmtree
 
 REPO = Path(__file__).resolve().parent.parent
-TMPDIR = REPO / 'tmp'
+TMPDIR = REPO / "tmp"
+
 
 def all_tests(testmodule):
+    """ Iterator[Tuple[str, Callable]]: Methods to call in testmodule. """
     for name, obj in vars(testmodule).items():
-        if name.startswith('test_') and callable(obj):
+        if name.startswith("test_") and callable(obj):
             yield name, obj
+
 
 def bigprint(**kwargs):
     """ None: Fancy print function for key:value pairs. """
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         print(f"{k}: {repr(v)}")
+
 
 def cleartmp():
     """ None: Delete and recreate temporary folder. """
@@ -24,6 +28,7 @@ def cleartmp():
 
     TMPDIR.mkdir()
 
+
 def do(meth):
     """ None or Exception: There is no try. """
     try:
@@ -31,14 +36,11 @@ def do(meth):
     except Exception as err:
         return err
 
+
 def errors(module):
+    """ dict: {name:Exception or None} for tests in module. """
     modname = module.__name__
-    errors = ( (k, do(v)) for k,v in all_tests(module) )
-    errors = ( (k, v) for k,v in errors if v is not None )
+    errors = ((k, do(v)) for k, v in all_tests(module))
+    errors = ((k, v) for k, v in errors if v is not None)
 
-    return { f"{modname}.{name}":err for name, err in errors }
-
-
-
-
-
+    return {f"{modname}.{name}": err for name, err in errors}
